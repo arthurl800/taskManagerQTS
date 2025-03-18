9fdecd0b7f9aabcdc9329d62de265f90dbfeea8d
AL800 <arthur.mfdl@gmail.com>
2025-03-18 12:57:39 -0300

    Ajustes 2025_03_18 12:57

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '41c0252c4a100d3b715d7d2be0e4935de97e89a2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '41c0252c4a100d3b715d7d2be0e4935de97e89a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
